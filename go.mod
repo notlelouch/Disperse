72ce95d38f4e2f9dc2d59b2fb13cbab1d20cf10c
notlelouch <notlelouchvb@gmail.com>
2024-10-13 03:08:58 +0530

    implemented key-value cache

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/notlelouch/Distributed-Cache.git
+
+go 1.23.2
---
+++ pkg/cache/cache.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type CacheItem struct {
+	Key        string
+	Value      interface{}
+	Expiration int64
+}
+
+type Cache struct {
+	items map[string]CacheItem
+	mu    sync.RWMutex
+}
+
+func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	expiration := time.Now().Add(duration).UnixNano()
+	c.items[key] = CacheItem{
+		Key:        key,
+		Value:      value,
+		Expiration: expiration,
+	}
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	item, found := c.items[key]
+	if !found || item.Expiration <= time.Now().UnixNano() {
+		return nil, false
+	}
+	return item.Value, true
+}
+
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.items, key)
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		items: make(map[string]CacheItem),
+	}
+}
