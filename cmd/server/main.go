ec14e14f2d14877787e68573d0439d5a43c79a9b
notlelouch <notlelouchvb@gmail.com>
2024-11-01 18:38:18 +0530

    Implemented Fiber instead of net/http and separated the HTTP (Fiber) port from the Memberlist port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,24 +3,28 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/notlelouch/Distributed-Cache/pkg/distributed"
 )
 
 func main() {
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		log.Fatalf("Invalid PORT: %v", port)
-	}
+	// Separating the HTTP (Fiber) port from the Memberlist port:
+	// port, err := strconv.Atoi(os.Getenv("PORT"))
+	httpPort, _ := strconv.Atoi(os.Getenv("HTTP_PORT"))
+	memberlistPort, _ := strconv.Atoi(os.Getenv("PORT"))
+	// if err != nil {
+	// 	log.Fatalf("Invalid PORT: %v", port)
+	// }
 
 	node_name := os.Getenv("NODE_NAME")
 
 	peer := os.Getenv("PEER")
 
-	dc, err := distributed.NewDistributedCache(port, node_name)
+	// dc, err := distributed.NewDistributedCache(port, node_name)
+	dc, err := distributed.NewDistributedCache(memberlistPort, node_name)
 	if err != nil {
 		log.Fatalf("Failed to create distributed cache: %v", err)
 	}
@@ -34,8 +38,18 @@ func main() {
 		}
 	}
 
-	http.HandleFunc("/cache/", dc.HTTPHandler)
-	log.Printf("Server is running on port: %d", port)
+	// // net/http Handlers
+	// http.HandleFunc("/cache/", dc.HTTPHandler)
+	// log.Printf("Server is running on port: %d", port)
+	// log.Print(dc.Config.Name)
+	// log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+
+	// Fiber Handler
+	app := fiber.New()
+	app.All("/cache/:key", dc.FiberHandler)
+
+	log.Printf("Server is running on port: %d", httpPort)
 	log.Print(dc.Config.Name)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	// log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", httpPort)))
 }
--- pkg/distributed/distributed.go
+++ pkg/distributed/distributed.go
@@ -3,10 +3,10 @@ package distributed
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/hashicorp/memberlist"
 	"github.com/notlelouch/Distributed-Cache/pkg/cache"
 )
@@ -17,17 +17,20 @@ type DistributedCache struct {
 	Config *memberlist.Config
 }
 
-func NewDistributedCache(port int, node_name string) (*DistributedCache, error) {
+func NewDistributedCache(memberlistPort int, node_name string) (*DistributedCache, error) {
 	// Initialize the local cache
 	cacheInstance := cache.NewCache()
 	config := memberlist.DefaultLocalConfig()
 	config.Name = node_name
 	config.BindAddr = "127.0.0.1"
-	config.BindPort = port
-	config.AdvertiseAddr = "127.0.0.1"
-	config.AdvertisePort = port
 
-	// Configure memberlist with de ault LAN settings and custom port
+	// config.BindPort = port
+	config.BindPort = memberlistPort // Use different port for memberlist
+	config.AdvertiseAddr = "127.0.0.1"
+
+	// config.AdvertisePort = port
+	config.AdvertisePort = memberlistPort
+	// Configure memberlist with default LAN settings and custom port
 	// config := memberlist.DefaultLANConfig()
 	// config.BindPort = port
 	// config.AdvertisePort = port
@@ -75,39 +78,75 @@ func (dc *DistributedCache) JoinCluster(peer string) error {
 	return err
 }
 
-// net/http Handlers
-func (dc *DistributedCache) HTTPHandler(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Path[len("/cache/"):]
-	switch r.Method {
-	case http.MethodGet:
+// // net/http Handlers
+// func (dc *DistributedCache) HTTPHandler(w http.ResponseWriter, r *http.Request) {
+// 	key := r.URL.Path[len("/cache/"):]
+// 	switch r.Method {
+// 	case http.MethodGet:
+// 		log.Printf("METHODGET#####")
+// 		value, found := dc.Cache.Get(key)
+// 		if !found {
+// 			w.WriteHeader(http.StatusNotFound)
+// 			return
+// 		}
+// 		fmt.Fprintf(w, "%v", value)
+// 		log.Printf("value of %s is %s", key, value)
+//
+// 	case http.MethodPut:
+// 		log.Printf("METHODEPUT#####")
+// 		value := r.PostFormValue("value")
+// 		durationStr := r.PostFormValue("duration")
+// 		duration, err := strconv.ParseInt(durationStr, 10, 64)
+// 		if err != nil {
+// 			w.WriteHeader(http.StatusBadRequest)
+// 			return
+// 		}
+// 		dc.Cache.Set(key, value, time.Duration(duration))
+// 		log.Printf("Successfully Set %s to %s", key, value)
+//
+// 	case http.MethodDelete:
+// 		log.Printf("METHODEDELETE####")
+// 		dc.Cache.Delete(key)
+//
+// 	default:
+// 		w.WriteHeader(http.StatusMethodNotAllowed)
+// 		fmt.Fprintf(w, "Method not allowed")
+// 		log.Printf("Successfully delete %s", key)
+// 	}
+// }
+
+// Fiber Handler
+func (dc *DistributedCache) FiberHandler(c *fiber.Ctx) error {
+	key := c.Params("key")
+	switch c.Method() {
+	case "GET":
 		log.Printf("METHODGET#####")
 		value, found := dc.Cache.Get(key)
 		if !found {
-			w.WriteHeader(http.StatusNotFound)
-			return
+			return c.SendStatus(fiber.StatusNotFound)
 		}
-		fmt.Fprintf(w, "%v", value)
 		log.Printf("value of %s is %s", key, value)
+		return c.SendString(fmt.Sprintf("%v", value))
 
-	case http.MethodPut:
+	case "PUT":
 		log.Printf("METHODEPUT#####")
-		value := r.PostFormValue("value")
-		durationStr := r.PostFormValue("duration")
+		value := c.FormValue("value")
+		durationStr := c.FormValue("duration")
 		duration, err := strconv.ParseInt(durationStr, 10, 64)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
+			return c.SendStatus(fiber.StatusBadRequest)
 		}
 		dc.Cache.Set(key, value, time.Duration(duration))
 		log.Printf("Successfully Set %s to %s", key, value)
+		return c.SendStatus(fiber.StatusOK)
 
-	case http.MethodDelete:
+	case "DELETE":
 		log.Printf("METHODEDELETE####")
 		dc.Cache.Delete(key)
+		log.Printf("Successfully delete %s", key)
+		return c.SendStatus(fiber.StatusOK)
 
 	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "Method not allowed")
-		log.Printf("Successfully delete %s", key)
+		return c.Status(fiber.StatusMethodNotAllowed).SendString("Method not allowed")
 	}
 }
